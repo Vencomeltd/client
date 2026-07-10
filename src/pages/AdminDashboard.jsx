ed7719a073d9a1b64103c4cbee3e5ef6feb662b0
Jayden Benson <jethertechnology@gmail.com>
2026-07-10 13:06:20 +0100

    fix: add code plugin to TinyMCE for source code editing

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -2349,7 +2349,7 @@ function ContentSection({ blogs, fetchBlogs, blogForm, setBlogForm, editingBlog,
       }
       window.tinymce.init({
         selector: "#blog-content-editor",
-        plugins: "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
+        plugins: "anchor autolink charmap codesample code emoticons image link lists media searchreplace table visualblocks wordcount",
         toolbar: "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat | code",
         height: 400,
         menubar: false,
