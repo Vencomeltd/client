761e3d10f03e3427737cf43e56ccb9a393371021
Jayden Benson <jethertechnology@gmail.com>
2026-07-10 12:59:58 +0100

    fix: add source code button to TinyMCE toolbar

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -2350,7 +2350,7 @@ function ContentSection({ blogs, fetchBlogs, blogForm, setBlogForm, editingBlog,
       window.tinymce.init({
         selector: "#blog-content-editor",
         plugins: "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
-        toolbar: "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat",
+        toolbar: "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat | code",
         height: 400,
         menubar: false,
         skin: "oxide",
